037902dbd2fb19bd72b6fca181abec30fa2e4ea7
Vitor Souza Piña <vitor.pina@glintt.com>
2026-01-20 15:16:18 +0000

    test in branch

--- dev/test_hello_world.py
+++ dev/test_hello_world.py
@@ -7,6 +7,11 @@ from airflow.operators.python import PythonOperator
 def _hello():
     print("### Hello from NAU Analytics DAG repo branch feat_spark_submit###")
 
+def _hellosub():
+    import subprocess
+    print(subprocess.check_output(["git", "branch", "--show-current"]).decode())
+
+
 
 with DAG(
     dag_id="test_hello_world",
@@ -19,3 +24,8 @@ with DAG(
         task_id="say_hello",
         python_callable=_hello,
     )
+    print_repo = PythonOperator(
+        task_id="print_repo_branch",
+        python_callable=_hellosub
+    )
+    hello >> print_repo
