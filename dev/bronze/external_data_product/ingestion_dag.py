5f19781c10af0de4d01d43e715de9a7dddf65f20
Vitor Souza Piña <vitor.pina@glintt.com>
2026-02-16 17:11:38 +0000

    WIP: adding one script per table

--- dev/bronze/external_data_product/ingestion_dag.py
+++ dev/bronze/external_data_product/ingestion_dag.py
@@ -119,6 +119,64 @@ def certificates_generatedcertificate_ingestion(cfg:dict) -> KubernetesPodOperat
     dag = cfg["dag"]
     )
 
+def grades_persistentcoursegrade_ingestion(cfg:dict) -> KubernetesPodOperator:
+    return KubernetesPodOperator(
+    namespace=cfg["namespace"],
+    service_account_name='spark-role',
+    # ✔ official spark image built for k8s
+    image='nauedu/nau-analytics-spark-shell:d465952',
+    startup_timeout_seconds=600,
+    # ✔ override entrypoint to run spark-submit
+    cmds=["/bin/bash", "-c"],
+    # ✔ submit a SparkPi example packaged inside the image
+    arguments=[
+        f"""
+            spark-submit \
+          --master k8s://https://kubernetes.default.svc:443 \
+          --deploy-mode cluster \
+          --name grades_persistentcoursegrade-ingestion \
+          --conf spark.kubernetes.container.image=nauedu/nau-analytics-external-data-product:feature-ingestion-script-improvements \
+          --conf spark.kubernetes.namespace={cfg["namespace"]} \
+          --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-role \
+          --conf spark.kubernetes.submission.waitAppCompletion=true \
+          --conf spark.executor.instances=2 \
+          --conf spark.executor.cores=1 \
+          --conf spark.executor.memory=8g \
+          --conf spark.kubernetes.driverEnv.MYSQL_DATABASE={cfg["database"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_HOST={cfg["host"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_PORT={cfg["port"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_USER={cfg["user"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_SECRET={cfg["secret"]} \
+          --conf spark.kubernetes.driverEnv.S3_ACCESS_KEY={cfg["S3_ACCESS_KEY"]} \
+          --conf spark.kubernetes.driverEnv.S3_SECRET_KEY={cfg["S3_SECRET_KEY"]} \
+          --conf spark.kubernetes.driverEnv.S3_ENDPOINT={cfg["S3_ENDPOINT"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_HOST={cfg["ICEBERG_CATALOG_HOST"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_PORT={cfg["ICEBERG_CATALOG_PORT"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_USER={cfg["ICEBERG_CATALOG_USER"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_PASSWORD={cfg["ICEBERG_CATALOG_PASSWORD"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_DATABASE_CATALOG_NAME={cfg["BRONZE_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_CATALOG_NAME={cfg["BRONZE_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_CATALOG_WAREHOUSE={cfg["BRONZE_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_DATABASE_CATALOG_NAME={cfg["SILVER_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_CATALOG_NAME={cfg["SILVER_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_CATALOG_WAREHOUSE={cfg["SILVER_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_DATABASE_CATALOG_NAME={cfg["GOLD_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_CATALOG_NAME={cfg["GOLD_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_CATALOG_WAREHOUSE={cfg["GOLD_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driver.service.deleteOnTermination=true \
+          --conf spark.kubernetes.executor.deleteOnTermination=true \
+          --conf spark.kubernetes.container.image.pullPolicy=Always \
+          local:///opt/spark/work-dir/src/bronze/python/bronze_grades_persistentcoursegrade_ingestion.py\
+          2>&1 | tee log.txt; LAST_EXIT=$(grep -Ei "exit code" log.txt | tail -n1 | sed 's/.*: *//'); echo "Parsed Spark exit code: $LAST_EXIT"; exit "$LAST_EXIT"
+        """
+    ],
+    name="grades_persistentcoursegrade_ingestion",
+    task_id="grades_persistentcoursegrade_ingestion_1",
+    get_logs=True,
+    on_finish_action="delete_pod",
+    dag = cfg["dag"]
+    )
+
 def get_connection_properties(dag: DAG)->dict:
     
     try:
@@ -185,6 +243,7 @@ cfg = get_connection_properties(bronze_dag)
 
 course_overviews_courseoverview_ingestion_task = course_overviews_courseoverview_ingestion(cfg=cfg)
 certificates_generatedcertificate_ingestion_task = certificates_generatedcertificate_ingestion(cfg=cfg)
+grades_persistentcoursegrade_ingestion_task = grades_persistentcoursegrade_ingestion(cfg=cfg)
 
 
-course_overviews_courseoverview_ingestion_task >> certificates_generatedcertificate_ingestion_task #type:ignore
+course_overviews_courseoverview_ingestion_task >> certificates_generatedcertificate_ingestion_task >> grades_persistentcoursegrade_ingestion_task #type:ignore
