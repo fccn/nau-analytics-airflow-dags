b4999fcb2af2cf20cf9d5d858f235594fde9a458
Vitor Souza Piña <vitor.pina@glintt.com>
2026-03-30 15:47:22 +0100

    Fix: adding ttl

--- stage/ingestion_dag.py
+++ stage/ingestion_dag.py
@@ -60,7 +60,7 @@ def course_overviews_courseoverview_ingestion(cfg:dict) -> KubernetesPodOperator
         f"""
             spark-submit \
           --master k8s://https://kubernetes.default.svc:443 \
-          --deploy-mode client \
+          --deploy-mode cluster \
           --name course_overviews_courseoverview-ingestion \
           --conf spark.kubernetes.container.image={cfg['docker_image']} \
           --conf spark.kubernetes.namespace={cfg["namespace"]} \
@@ -69,41 +69,36 @@ def course_overviews_courseoverview_ingestion(cfg:dict) -> KubernetesPodOperator
           --conf spark.executor.instances=2 \
           --conf spark.executor.cores=1 \
           --conf spark.executor.memory=8g \
+          --conf spark.kubernetes.driverEnv.ENVIRONMENT={cfg["ENVIRONMENT"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_DATABASE={cfg["database"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_HOST={cfg["host"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_PORT={cfg["port"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_USER={cfg["user"]} \
+          --conf spark.kubernetes.driverEnv.MYSQL_SECRET={cfg["secret"]} \
+          --conf spark.kubernetes.driverEnv.S3_ACCESS_KEY={cfg["S3_ACCESS_KEY"]} \
+          --conf spark.kubernetes.driverEnv.S3_SECRET_KEY={cfg["S3_SECRET_KEY"]} \
+          --conf spark.kubernetes.driverEnv.S3_ENDPOINT={cfg["S3_ENDPOINT"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_HOST={cfg["ICEBERG_CATALOG_HOST"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_PORT={cfg["ICEBERG_CATALOG_PORT"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_USER={cfg["ICEBERG_CATALOG_USER"]} \
+          --conf spark.kubernetes.driverEnv.ICEBERG_CATALOG_PASSWORD={cfg["ICEBERG_CATALOG_PASSWORD"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_DATABASE_CATALOG_NAME={cfg["BRONZE_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_CATALOG_NAME={cfg["BRONZE_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.BRONZE_ICEBERG_CATALOG_WAREHOUSE={cfg["BRONZE_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_DATABASE_CATALOG_NAME={cfg["SILVER_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_CATALOG_NAME={cfg["SILVER_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.SILVER_ICEBERG_CATALOG_WAREHOUSE={cfg["SILVER_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_DATABASE_CATALOG_NAME={cfg["GOLD_ICEBERG_DATABASE_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_CATALOG_NAME={cfg["GOLD_ICEBERG_CATALOG_NAME"]} \
+          --conf spark.kubernetes.driverEnv.GOLD_ICEBERG_CATALOG_WAREHOUSE={cfg["GOLD_ICEBERG_CATALOG_WAREHOUSE"]} \
+          --conf spark.kubernetes.driver.annotation."ttlSecondsAfterFinished"=60 \
           --conf spark.kubernetes.driver.service.deleteOnTermination=true \
-          --conf spark.driver.host=course-overviews-courseoverview-ingestion-driver \
-          --conf spark.driver.port=7078 \
-          --conf spark.blockManager.port=7079 \
-          --conf spark.kubernetes.driver.service.name=course-overviews-courseoverview-ingestion-driver \
           --conf spark.kubernetes.executor.deleteOnTermination=true \
           --conf spark.kubernetes.container.image.pullPolicy=Always \
           local:///opt/spark/work-dir/src/bronze/python/bronze_course_overviews_courseoverview_ingestion.py\
           2>&1 | tee log.txt; LAST_EXIT=$(grep -Ei "exit code" log.txt | tail -n1 | sed 's/.*: *//'); echo "Parsed Spark exit code: $LAST_EXIT"; exit "$LAST_EXIT"
         """
     ],
-    env_vars={
-        "ENVIRONMENT": cfg["ENVIRONMENT"],
-        "MYSQL_DATABASE": cfg["database"],
-        "MYSQL_HOST": cfg["host"],
-        "MYSQL_PORT": str(cfg["port"]),
-        "MYSQL_USER": cfg["user"],
-        "MYSQL_SECRET": cfg["secret"],
-        "S3_ACCESS_KEY": cfg["S3_ACCESS_KEY"],
-        "S3_SECRET_KEY": cfg["S3_SECRET_KEY"],
-        "S3_ENDPOINT": cfg["S3_ENDPOINT"],
-        "ICEBERG_CATALOG_HOST": cfg["ICEBERG_CATALOG_HOST"],
-        "ICEBERG_CATALOG_PORT": str(cfg["ICEBERG_CATALOG_PORT"]),
-        "ICEBERG_CATALOG_USER": cfg["ICEBERG_CATALOG_USER"],
-        "ICEBERG_CATALOG_PASSWORD": cfg["ICEBERG_CATALOG_PASSWORD"],
-        "BRONZE_ICEBERG_DATABASE_CATALOG_NAME": cfg["BRONZE_ICEBERG_DATABASE_CATALOG_NAME"],
-        "BRONZE_ICEBERG_CATALOG_NAME": cfg["BRONZE_ICEBERG_CATALOG_NAME"],
-        "BRONZE_ICEBERG_CATALOG_WAREHOUSE": cfg["BRONZE_ICEBERG_CATALOG_WAREHOUSE"],
-        "SILVER_ICEBERG_DATABASE_CATALOG_NAME": cfg["SILVER_ICEBERG_DATABASE_CATALOG_NAME"],
-        "SILVER_ICEBERG_CATALOG_NAME": cfg["SILVER_ICEBERG_CATALOG_NAME"],
-        "SILVER_ICEBERG_CATALOG_WAREHOUSE": cfg["SILVER_ICEBERG_CATALOG_WAREHOUSE"],
-        "GOLD_ICEBERG_DATABASE_CATALOG_NAME": cfg["GOLD_ICEBERG_DATABASE_CATALOG_NAME"],
-        "GOLD_ICEBERG_CATALOG_NAME": cfg["GOLD_ICEBERG_CATALOG_NAME"],
-        "GOLD_ICEBERG_CATALOG_WAREHOUSE": cfg["GOLD_ICEBERG_CATALOG_WAREHOUSE"],
-    },
     name="course_overviews_courseoverview_ingestion",
     task_id="course_overviews_courseoverview_ingestion_1",
     get_logs=True,
