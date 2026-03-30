e6abf1033d9858cd623f2fb1cb1f0ece38bbef73
Vitor Souza Piña <vitor.pina@glintt.com>
2026-03-30 18:29:42 +0100

    Fix: typo

--- stage/ingestion_dag_2.py
+++ stage/ingestion_dag_2.py
@@ -3,6 +3,50 @@ from datetime import datetime
 from airflow.sdk import Variable,Connection #type: ignore
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator #type: ignore
 
+def get_connection_properties(dag: DAG)->dict:
+    
+    try:
+        config = {}
+        config["docker_image"] = Variable.get("docker_image")
+        config["dag"] = dag
+        config["namespace"] = Variable.get("namespace")
+        
+        mysql_conn = Connection.get("sql_source_stage_connection")
+        config["database"] = mysql_conn.extra_dejson.get("mysqldatabase")
+        config["host"] = mysql_conn.host
+        config["user"] = mysql_conn.login
+        config["port"] = mysql_conn.port
+        config["secret"] = mysql_conn.password
+        
+
+        s3_conn = Connection.get("s3_stage_connection")
+        config["S3_ACCESS_KEY"] = s3_conn.login
+        config["S3_SECRET_KEY"] = s3_conn.password
+        config["S3_ENDPOINT"] =s3_conn.extra_dejson.get("s3endpoint")
+
+        iceberg_catalog_conn = Connection.get("iceberg_stage_connection")
+        config["ICEBERG_CATALOG_HOST"] =iceberg_catalog_conn.host
+        config["ICEBERG_CATALOG_PORT"] = iceberg_catalog_conn.port
+        config["ICEBERG_CATALOG_USER"] = iceberg_catalog_conn.login
+        config["ICEBERG_CATALOG_PASSWORD"] =iceberg_catalog_conn.password        
+       
+        config["BRONZE_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_database_catalog_name")
+        config["BRONZE_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_catalog_name")
+        config["BRONZE_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_catalog_warehouse")
+        
+        config["SILVER_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_database_catalog_name")
+        config["SILVER_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_catalog_name")
+        config["SILVER_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_catalog_warehouse")    
+
+        config["GOLD_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_database_catalog_name")
+        config["GOLD_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_catalog_name")
+        config["GOLD_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_catalog_warehouse")
+        
+        config["ENVIRONMENT"] = Variable.get("ENVIRONMENT")
+        return config
+    except Exception:
+        raise Exception(f"Could not get the variables or secrets: {Exception}")
+    
 def organizations_organization_ingestion(cfg:dict) -> KubernetesPodOperator:
     return KubernetesPodOperator(
     namespace=cfg["namespace"],
@@ -239,49 +283,7 @@ def student_courseenrollment_ingestion(cfg:dict) -> KubernetesPodOperator:
     dag = cfg["dag"]
     )
 
-def get_connection_properties(dag: DAG)->dict:
-    
-    try:
-        config = {}
-        config["docker_image"] = Variable.get("docker_image")
-        config["ENVIRONMENT"] = Variable.get("ENVIRONMENT")
-        config["dag"] = dag
-        config["namespace"] = "dev-nau-analytics"
-        
-        mysql_conn = Connection.get("sql_source_dev_connection")
-        config["database"] = mysql_conn.extra_dejson.get("mysqldatabase")
-        config["host"] = mysql_conn.host
-        config["user"] = mysql_conn.login
-        config["port"] = mysql_conn.port
-        config["secret"] = mysql_conn.password
-        
 
-        s3_conn = Connection.get("s3_dev_connection")
-        config["S3_ACCESS_KEY"] = s3_conn.login
-        config["S3_SECRET_KEY"] = s3_conn.password
-        config["S3_ENDPOINT"] =s3_conn.extra_dejson.get("s3endpoint")
-
-        iceberg_catalog_conn = Connection.get("iceberg_dev_connection")
-        config["ICEBERG_CATALOG_HOST"] =iceberg_catalog_conn.host
-        config["ICEBERG_CATALOG_PORT"] = iceberg_catalog_conn.port
-        config["ICEBERG_CATALOG_USER"] = iceberg_catalog_conn.login
-        config["ICEBERG_CATALOG_PASSWORD"] =iceberg_catalog_conn.password        
-       
-        config["BRONZE_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_database_catalog_name")
-        config["BRONZE_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_catalog_name")
-        config["BRONZE_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("bronze_iceberg_catalog_warehouse")
-        
-        config["SILVER_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_database_catalog_name")
-        config["SILVER_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_catalog_name")
-        config["SILVER_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("silver_iceberg_catalog_warehouse")    
-
-        config["GOLD_ICEBERG_DATABASE_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_database_catalog_name")
-        config["GOLD_ICEBERG_CATALOG_NAME"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_catalog_name")
-        config["GOLD_ICEBERG_CATALOG_WAREHOUSE"] = iceberg_catalog_conn.extra_dejson.get("gold_iceberg_catalog_warehouse")
-        
-        return config
-    except Exception:
-        raise Exception(f"Could not get the variables or secrets: {Exception}")
 
 
 
