9d52501be2c0e97a509bf58b99fd940d15d21b2b
paulormonteiro <paulo.r.monteiro@glinttglobal.com>
2026-04-06 11:28:03 +0100

    Fixed gold resource allocation

--- stage/gold_dag.py
+++ stage/gold_dag.py
@@ -46,7 +46,7 @@ def get_connection_properties(dag: DAG)->dict:
         raise Exception(f"Could not get the variables or secrets: {Exception}")
 
 
-def make_gold_operator(cfg: dict, name: str, script: str, executor_cores: int = 2, pod_image=None) -> KubernetesPodOperator:
+def make_gold_operator(cfg: dict, name: str, script: str, executor_cores: int = 4, pod_image=None) -> KubernetesPodOperator:
     image = pod_image or cfg["docker_image"]
     return KubernetesPodOperator(
         namespace=cfg["namespace"],
@@ -64,7 +64,9 @@ def make_gold_operator(cfg: dict, name: str, script: str, executor_cores: int = 
           --conf spark.kubernetes.namespace={cfg["namespace"]} \
           --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-role \
           --conf spark.kubernetes.submission.waitAppCompletion=true \
-          --conf spark.executor.instances=1 \
+          --conf spark.driver.cores=2 \
+          --conf spark.driver.memory=16g \
+          --conf spark.executor.instances=2 \
           --conf spark.executor.cores={executor_cores} \
           --conf spark.executor.memory=8g \
           --conf spark.kubernetes.driverEnv.ENVIRONMENT={cfg["ENVIRONMENT"]} \
