5aa525c1787779df7a9f142d0996cf12b7f04498
Vitor Souza Piña <vitor.pina@glintt.com>
2026-04-09 16:01:22 +0100

    fix: typo

--- prod/management_data_product/management_dag.py
+++ prod/management_data_product/management_dag.py
@@ -21,7 +21,7 @@ def get_connection_properties(dag: DAG) -> dict:
             "ENVIRONMENT": Variable.get("ENVIRONMENT"),
             "GOOGLE_ACCOUNT_JSON": base64.b64encode(json.dumps(json.loads(google_string_connection.password)).encode()).decode(),
             "GOOGLE_SHEET_ID":Variable.get("JIRA_GOOGLE_SHEET_ID"),
-            "DOWNTIMES_GOOGLE_SHEET_ID":Variable.get("DOWNTIMES_GOOGLE_SHEET_ID"),
+            "DOWNTIMES_GOOGLE_SHEET_ID": base64.b64encode(Variable.get("DOWNTIMES_GOOGLE_SHEET_ID").encode()).decode(),
             "S3_ACCESS_KEY": s3_conn.login,
             "S3_SECRET_KEY": s3_conn.password,
             "S3_ENDPOINT": s3_conn.extra_dejson.get("s3endpoint"),
